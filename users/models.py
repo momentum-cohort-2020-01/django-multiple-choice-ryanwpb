838bedd1ee57cb07911ab75769b4bed935da5bb9
Ryan Graham <ryangrahamwpb@gmail.com>
2020-03-09 17:48:38 -0400

    Add models and make migrations.

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 from django.contrib.auth.models import AbstractUser
 
 # Consider creating a custom user model from scratch as detailed at
@@ -7,3 +8,31 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Snippet(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    code_block = models.TextField(max_length=None)
+    language = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        "User", on_delete=models.CASCADE, null=True, blank=True)
+    tag = models.ManyToManyField('Tag')
+
+    def __str__(self):
+
+        return f"Snippet title {self.title} Description {self.description} code block {self.code_block} language {self.language} tag {self.tag} user {self.user}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40)
+ # trying to add slug
+    slug = models.SlugField(null=False, unique=True, default=slugify(name))
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
