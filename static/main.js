bf3802330a8abb05b9c52d3b33f4261e676fd0f8
Ryan Graham <ryangrahamwpb@gmail.com>
2020-03-10 12:52:59 -0400

    Add copy to clipboard js. Works... ish.

--- static/main.js
+++ static/main.js
@@ -1,1 +1,13 @@
-console.log("here");
+document.querySelector("code").addEventListener("click", async event => {
+  if (!navigator.clipboard) {
+    // Clipboard API not available
+    return;
+  }
+  const text = event.target.innerText;
+  try {
+    await navigator.clipboard.writeText(text);
+    event.target.textContent = "Copied to clipboard";
+  } catch (err) {
+    console.error("Failed to copy!", err);
+  }
+});
