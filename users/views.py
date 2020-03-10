35fef4884c6859ff8bf76a84153905aff9a7dcca
Ryan Graham <ryangrahamwpb@gmail.com>
2020-03-10 09:52:54 -0400

    Add decorator for required login.

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
 from .forms import SnippetForm
 
